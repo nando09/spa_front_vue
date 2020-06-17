85d3db572b22117bee2ba80296b6fbac974d988b
Fernando <nando.online@live.com>
2020-06-16 23:01:43 -0300

    Adicionando header e footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
 <template>
 	<div id="app">
-		<router-view/>
+		<header>
+			<nav-bar logo="Social" url="#" cor="green darken-1"/>
+		</header>
+		<main>
+			<div class="container">
+				<div class="row">
+					<grid-vue tamanho="4">
+						Menu
+					</grid-vue>
+					<grid-vue tamanho="8">
+						<router-view/>
+					</grid-vue>
+				</div>
+			</div>
+		</main>
+		<footer-vue cor="green darken-1" logo="Social" descricao="Teste descrição" ano="2020">
+			<li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+			<li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+			<li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+			<li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+		</footer-vue>
 	</div>
 </template>
 
 <script>
+import NavBar from '@/components/layouts/NavBar'
+import FooterVue from '@/components/layouts/FooterVue'
+import GridVue from '@/components/layouts/GridVue'
+
 export default {
-	name: 'app'
+	name: 'app',
+	components:{
+		NavBar,
+		FooterVue,
+		GridVue
+	}
 };
 </script>
 
