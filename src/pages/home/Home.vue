bb7cbf687d3cbd79f3af191c3adcbea5e591cc85
Fernando <nando.online@live.com>
2020-06-16 21:51:26 -0300

    Adicionando materialize CSS

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,6 +1,23 @@
 <template>
 	<div class="">
-		<h2>Social</h2>
+	<h2>Social</h2>
+
+	<div class="row">
+		<div class="col s12 m6">
+			<div class="card blue-grey darken-1">
+			<div class="card-content white-text">
+				<span class="card-title">Card Title</span>
+				<p>I am a very simple card. I am good at containing small bits of information.
+				I am convenient because I require little markup to use effectively.</p>
+			</div>
+			<div class="card-action">
+				<a href="#">This is a link</a>
+				<a href="#">This is a link</a>
+			</div>
+			</div>
+		</div>
+		</div>
+
 	</div>
 </template>
 
@@ -8,13 +25,14 @@
 export default {
 	name: 'Home',
 	data () {
-		return {
-		}
+	return {
+
 	}
-};
+	}
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-	
+
 </style>
